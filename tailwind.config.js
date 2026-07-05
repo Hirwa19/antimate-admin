34542d71d3ba74b6a2c73d4e7bb409a363d54a90
Hirwa19 <hirwasalem19@gmail.com>
2026-07-05 15:25:23 +0200

    update styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
-
   theme: {
     extend: {},
   },
-
   plugins: [],
-}
+};
